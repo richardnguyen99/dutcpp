c9c4603e1f3259b1d368d94af7d1b3bbe2e0c017
Richard Nguyen <richard@richardhnguyen.com>
2023-06-21 10:37:11 -0700

    Add clang-format

--- include/vector.h
+++ include/vector.h
@@ -2,7 +2,7 @@
  * @file vector.h
  * @author Richard Nguyen (richard@richardhnguyen.com)
  * @brief An implement of dynamic array for education purpose only
- * @version 0.1
+ * @version 0.2
  * @date 2023-06-20
  *
  * @copyright Copyright (c) 2023
@@ -12,9 +12,9 @@
 #define __DUTCPP_VECTOR_H 1
 
 #include <cstddef>
-#include <memory>
 #include <initializer_list>
 #include <iterator>
+#include <memory>
 
 namespace dutcpp
 {
@@ -34,81 +34,102 @@ public:
     using reference         = typename traits_type::reference;
     using pointer           = typename traits_type::pointer;
 
-    constexpr __normal_iterator() noexcept : _current(_Pointer()) {}
+    constexpr __normal_iterator() noexcept : _current(_Pointer()) { }
 
-    explicit __normal_iterator(const _Pointer &_p) noexcept : _current(_p) {}
+    explicit __normal_iterator(const _Pointer &_p) noexcept : _current(_p) { }
 
     template <typename _Iter>
     __normal_iterator(
         const __normal_iterator<
-            _Iter, typename std::enable_if<
-                        (std::is_same<
-                            _Iter, typename _Container::pointer>::__value),
-                        _Container>::__type> &_i) noexcept
-        : _current(_i._current)
+            _Iter,
+            typename std::enable_if<
+                (std::is_same<_Iter, typename _Container::pointer>::__value),
+                _Container>::__type> &_i) noexcept
+    : _current(_i._current)
     {
     }
 
-    reference operator*() const noexcept { return *_current; }
+    reference
+    operator*() const noexcept
+    {
+        return *_current;
+    }
 
-    pointer operator->() const noexcept { return _current; }
+    pointer
+    operator->() const noexcept
+    {
+        return _current;
+    }
 
-    __normal_iterator &operator++() noexcept
+    __normal_iterator &
+    operator++() noexcept
     {
         ++_current;
         return *this;
     }
 
-    __normal_iterator operator++(int) noexcept
+    __normal_iterator
+    operator++(int) noexcept
     {
         return __normal_iterator(_current++);
     }
 
-    __normal_iterator &operator--() noexcept
+    __normal_iterator &
+    operator--() noexcept
     {
         --_current;
         return *this;
     }
 
-    __normal_iterator operator--(int) noexcept
+    __normal_iterator
+    operator--(int) noexcept
     {
         return __normal_iterator(_current--);
     }
 
-    reference operator[](difference_type pos) const noexcept
+    reference
+    operator[](difference_type pos) const noexcept
     {
         return _current[pos];
     }
 
-    __normal_iterator &operator+=(difference_type n) noexcept
+    __normal_iterator &
+    operator+=(difference_type n) noexcept
     {
         _current += n;
         return *this;
     }
 
-    __normal_iterator operator+(difference_type n) const noexcept
+    __normal_iterator
+    operator+(difference_type n) const noexcept
     {
         return __normal_iterator(_current + n);
     }
 
-    __normal_iterator &operator-=(difference_type n) noexcept
+    __normal_iterator &
+    operator-=(difference_type n) noexcept
     {
         _current -= n;
         return *this;
     }
 
-    __normal_iterator operator-(difference_type n) const noexcept
+    __normal_iterator
+    operator-(difference_type n) const noexcept
     {
         return ____normal_iterator(_current - n);
     }
 
-    const _Pointer &base() const noexcept { return _current; }
+    const _Pointer &
+    base() const noexcept
+    {
+        return _current;
+    }
 };
 
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline bool
 operator==(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+           const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
 {
     return lhs.base() == rhs.base();
 }
@@ -116,7 +137,7 @@ operator==(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline bool
 operator==(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+           const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() == rhs.base();
 }
@@ -124,7 +145,7 @@ operator==(const __normal_iterator<_Iterator, _Container> &lhs,
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline bool
 operator!=(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+           const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
 {
     return lhs.base() != rhs.base();
 }
@@ -132,7 +153,7 @@ operator!=(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline bool
 operator!=(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+           const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() != rhs.base();
 }
@@ -140,7 +161,7 @@ operator!=(const __normal_iterator<_Iterator, _Container> &lhs,
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline bool
 operator<(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+          const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
 {
     return lhs.base() < rhs.base();
 }
@@ -148,7 +169,7 @@ operator<(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline bool
 operator<(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+          const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() < rhs.base();
 }
@@ -156,7 +177,7 @@ operator<(const __normal_iterator<_Iterator, _Container> &lhs,
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline bool
 operator<=(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+           const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
 {
     return lhs.base() <= rhs.base();
 }
@@ -164,7 +185,7 @@ operator<=(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline bool
 operator<=(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+           const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() <= rhs.base();
 }
@@ -172,7 +193,7 @@ operator<=(const __normal_iterator<_Iterator, _Container> &lhs,
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline bool
 operator>(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+          const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
 {
     return lhs.base() > rhs.base();
 }
@@ -180,7 +201,7 @@ operator>(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline bool
 operator>(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+          const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() > rhs.base();
 }
@@ -188,7 +209,7 @@ operator>(const __normal_iterator<_Iterator, _Container> &lhs,
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline bool
 operator>=(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+           const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
 {
     return lhs.base() >= rhs.base();
 }
@@ -196,7 +217,7 @@ operator>=(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline bool
 operator>=(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+           const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() >= rhs.base();
 }
@@ -204,7 +225,7 @@ operator>=(const __normal_iterator<_Iterator, _Container> &lhs,
 template <typename _IteratorL, typename _IteratorR, typename _Container>
 inline auto
 operator-(const __normal_iterator<_IteratorL, _Container> &lhs,
-            const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
+          const __normal_iterator<_IteratorR, _Container> &rhs) noexcept
     -> decltype(lhs.base() - rhs.base())
 {
     return lhs.base() - rhs.base();
@@ -213,56 +234,52 @@ operator-(const __normal_iterator<_IteratorL, _Container> &lhs,
 template <typename _Iterator, typename _Container>
 inline typename __normal_iterator<_Iterator, _Container>::difference_type
 operator-(const __normal_iterator<_Iterator, _Container> &lhs,
-            const __normal_iterator<_Iterator, _Container> &rhs) noexcept
+          const __normal_iterator<_Iterator, _Container> &rhs) noexcept
 {
     return lhs.base() - rhs.base();
 }
 
 template <typename _Iterator, typename _Container>
-inline __normal_iterator<_Iterator, _Container> operator+(
-    typename __normal_iterator<_Iterator, _Container>::difference_type n,
-    const __normal_iterator<_Iterator, _Container>                    &i)
+inline __normal_iterator<_Iterator, _Container>
+operator+(typename __normal_iterator<_Iterator, _Container>::difference_type n,
+          const __normal_iterator<_Iterator, _Container> &i)
 {
     return __normal_iterator<_Iterator, _Container>(i.base() + n);
 }
-
 
 template <typename Tp>
 class vector
 {
 public:
     // Define types for consistency
-    using value_type = Tp;
-    using reference = Tp &;
+    using value_type      = Tp;
+    using reference       = Tp &;
     using const_reference = const Tp &;
-    using pointer = Tp*;
-    using const_pointer = const Tp*;
-    using size_type = std::size_t;
+    using pointer         = Tp *;
+    using const_pointer   = const Tp *;
+    using size_type       = std::size_t;
     using difference_type = std::ptrdiff_t;
 
     using allocator = std::allocator<Tp>;
-    using traits_t = std::allocator_traits<allocator>;
+    using traits_t  = std::allocator_traits<allocator>;
 
-    using iterator = __normal_iterator<pointer, vector>;
-    using const_iterator = __normal_iterator<const_pointer, vector>;
-    using reverse_iterator = std::reverse_iterator<iterator>;
+    using iterator               = __normal_iterator<pointer, vector>;
+    using const_iterator         = __normal_iterator<const_pointer, vector>;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     /**
      * @brief Default constructor
-     * 
+     *
      * This constructor will construct a vector with zero capacity. New pushing
      * will do the first allocation.
      */
-    vector() 
-    : _start(), _finish(), _end() 
-    {
-    }
+    vector() : _start(), _finish(), _end() { }
 
     /**
      * @brief Default fill constructor
-     * 
-     * This constructor will construct a vector and fill in the vector with 
+     *
+     * This constructor will construct a vector and fill in the vector with
      * %count elements. The value of all elements are the default value defined
      * by %value_type.
      */
@@ -271,67 +288,65 @@ public:
         // Call to value_type() will invoke the default value for value_type.
         // For example, if value_type (Tp) is an int, calling int() will be 0.
         // For customized object, the value_type object must support default
-        // constructor. 
+        // constructor.
         _fill_initialize(count, value_type());
     }
 
     /**
      * @brief Fill constructor with specified value
-     * 
+     *
      * Same as default fill constructor. But instead of filling default value,
      * the value of filled elements is a copy of the parameter %value.
      */
-    explicit vector (size_type count, const_reference value)
+    explicit vector(size_type count, const_reference value)
     {
         _fill_initialize(count, value);
     }
 
     /**
      * @brief Copy constructor
-     * 
-     * This constructor will construct a new vector object and copy the data 
+     *
+     * This constructor will construct a new vector object and copy the data
      * from the %other vector to this newly-created vector. The original vector
      * is guaranteed to have its data unmodified.
      */
-    vector(const vector& other) 
+    vector(const vector &other)
     {
         _range_initialize(std::cbegin(other), std::cend(other));
     }
 
     /**
      * @brief Move constructor
-     * 
-     * This constructor will construct a new vector object and transfer the 
-     * ownership from the %other vector to the newly-created vector. It 
+     *
+     * This constructor will construct a new vector object and transfer the
+     * ownership from the %other vector to the newly-created vector. It
      * guarantees there is no copy happening.
      */
     vector(vector &&other)
     {
-        this->_start = other._start;
+        this->_start  = other._start;
         this->_finish = other._finish;
-        this->_end = other._end;
+        this->_end    = other._end;
 
-        other._start = pointer();
+        other._start  = pointer();
         other._finish = pointer();
-        other._end = pointer();
+        other._end    = pointer();
     }
 
     /**
      * @brief Range constructor
-     * 
-     * This constructor will construct a new vector object and fill in the 
-     * vector with the data in the iterator range [first, last). 
-     * 
+     *
+     * This constructor will construct a new vector object and fill in the
+     * vector with the data in the iterator range [first, last).
+     *
      * If a container supports iterator methods such `begin()` and `end()`, it
-     * will behave just like copy constructor, but the other object can be a 
+     * will behave just like copy constructor, but the other object can be a
      * different class.
      */
-    template <class InputIter, 
-        typename = typename ::std::enable_if<std::is_convertible<
-            typename std::iterator_traits<InputIter>::iterator_category,
-            std::input_iterator_tag
-        >::value>::type
-    >
+    template <class InputIter,
+              typename = typename ::std::enable_if<std::is_convertible<
+                  typename std::iterator_traits<InputIter>::iterator_category,
+                  std::input_iterator_tag>::value>::type>
     vector(InputIter first, InputIter last)
     {
         // The reason why we need to add type check on InputIter is to remove
@@ -343,8 +358,8 @@ public:
 
     /**
      * @brief List constructor
-     * 
-     * This constructor will construct a new vector object and fill in the 
+     *
+     * This constructor will construct a new vector object and fill in the
      * vector with the data from the %init list.
      */
     vector(std::initializer_list<value_type> init)
@@ -352,12 +367,13 @@ public:
         _range_initialize(init.begin(), init.end());
     }
 
-    ~vector() 
+    ~vector()
     {
         const difference_type n = std::distance(this->_start, this->_finish);
 
         for (auto curr = this->_start; curr != this->_finish; ++curr)
-            // See https://stackoverflow.com/questions/14820307/when-to-use-addressofx-instead-of-x
+            // See
+            // https://stackoverflow.com/questions/14820307/when-to-use-addressofx-instead-of-x
             traits_t::destroy(_alloc, std::addressof(*curr));
 
         _finish = _start;
@@ -365,40 +381,40 @@ public:
     }
 
     /**
-     * @brief Returns a read/write iterator that points to the first element in 
+     * @brief Returns a read/write iterator that points to the first element in
      * the vector
      */
-    iterator 
+    iterator
     begin()
     {
         return iterator(this->_start);
     }
 
     /**
-     * @brief Returns a read iterator that points to the first element in the 
+     * @brief Returns a read iterator that points to the first element in the
      * vector
      */
-    const_iterator 
+    const_iterator
     begin() const
     {
         return const_iterator(this->_start);
     }
 
     /**
-     * @brief Returns a read iterator that points to the first element in the 
+     * @brief Returns a read iterator that points to the first element in the
      * vector
      */
-    const_iterator 
+    const_iterator
     cbegin() const
     {
         return const_iterator(this->_start);
     }
 
     /**
-     * @brief Returns a read/write iterator that points to one-past the last 
+     * @brief Returns a read/write iterator that points to one-past the last
      * element in the vector
      */
-    iterator 
+    iterator
     end()
     {
         return iterator(this->_finish);
@@ -408,7 +424,7 @@ public:
      * @brief Returns a read iterator that points to one-past the last element
      * in the vector
      */
-    const_iterator 
+    const_iterator
     end() const
     {
         return const_iterator(this->_finish);
@@ -418,17 +434,17 @@ public:
      * @brief Returns a read iterator that points to one-past the last element
      * in the vector
      */
-    const_iterator 
+    const_iterator
     cend() const
     {
         return const_iterator(this->_finish);
     }
 
     /**
-     * @brief Returns a read/write reversed iterator that points to one-past the 
+     * @brief Returns a read/write reversed iterator that points to one-past the
      * last element in the vector
      */
-    reverse_iterator 
+    reverse_iterator
     rbegin()
     {
         return reverse_iterator(end());
@@ -469,7 +485,7 @@ public:
      * in the vector
      */
     const_reverse_iterator
-    rend() const 
+    rend() const
     {
         return const_reverse_iterator(cbegin());
     }
@@ -479,7 +495,7 @@ public:
      * in the vector
      */
     const_reverse_iterator
-    crend() const 
+    crend() const
     {
         return const_reverse_iterator(cbegin());
     }
@@ -490,31 +506,31 @@ private:
     pointer _finish;
     pointer _end;
 
-
-private: 
-    void _fill_initialize(size_type count, const_reference value)
+private:
+    void
+    _fill_initialize(size_type count, const_reference value)
     {
-        this->_start = traits_t::allocate(_alloc, count);
+        this->_start  = traits_t::allocate(_alloc, count);
         this->_finish = this->_start;
-        this->_end = this->_start + count;
+        this->_end    = this->_start + count;
 
         for (size_type i = 0; i < count; ++i)
             traits_t::construct(_alloc, this->_finish++, value);
-
     }
 
-    template<class InputIter>
-    void _range_initialize(InputIter first, InputIter last)
+    template <class InputIter>
+    void
+    _range_initialize(InputIter first, InputIter last)
     {
-        size_type n = std::distance(first, last);
-        this->_start = traits_t::allocate(_alloc, n);
+        size_type n   = std::distance(first, last);
+        this->_start  = traits_t::allocate(_alloc, n);
         this->_finish = this->_start;
-        this->_end = this->_start + n;
+        this->_end    = this->_start + n;
 
         for (auto it = first; it != last; ++it)
             traits_t::construct(_alloc, this->_finish++, *it);
     }
 };
-}
+} // namespace dutcpp
 
 #endif
